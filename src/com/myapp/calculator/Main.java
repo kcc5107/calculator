5ed40a9154a766eba6e826cd9932d66b458f256e
Jang Gyeonghyuk <kcc5107@naver.com>
2025-04-16 16:07:46 +0900

    lv1 완료

--- src/com/myapp/calculator/Main.java
+++ src/com/myapp/calculator/Main.java
@@ -5,35 +5,44 @@ public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요 : ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요 : ");
-        int num2 = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요(+,-,*,/) : ");
-        char oper = sc.next().charAt(0);
+        while(true) {
+            System.out.print("첫 번째 숫자를 입력하세요 : ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요 : ");
+            int num2 = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요(+,-,*,/) : ");
+            char oper = sc.next().charAt(0);
 
-        int result = 0;
-        switch(oper) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if(num2==0) {
-                    System.out.println("나눗셈 연산에서 두 번째 숫자에 0이 입력될 수 없습니다.");
-                    return;
-                }
-                result = num1 / num2;
-                break;
-            default:
-                System.out.println("잘못된 사칙연산 기호입니다.");
+            int result = 0;
+            switch (oper) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 두 번째 숫자에 0이 입력될 수 없습니다.");
+                        continue;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("잘못된 사칙연산 기호입니다.");
+                    continue;
+            }
+
+            System.out.println("결과: " + result);
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
+            sc.nextLine();
+            String check = sc.nextLine();
+            if(check.equals("exit")) {
                 return;
+            }
         }
-        System.out.println("결과: " + result);
     }
 }
