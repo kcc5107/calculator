9692c3599a8d683bd061a32b5d6e707b12bc4f60
Jang Gyeonghyuk <kcc5107@naver.com>
2025-04-16 20:38:13 +0900

    lv2 완료

--- src/com/myapp/calculator/Calculator.java
+++ src/com/myapp/calculator/Calculator.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 public class Calculator {
     private ArrayList<Integer> results = new ArrayList<>();
 
+    // static 메서드로 써보려했으나 results가 인스턴스 필드이기때문에 static 필드로 바꿔줘야하는 문제발생
     public int calculator(int num1, int num2, char oper) {
         int result = 0;
+        boolean isChecked = false;
+
         switch (oper) {
             case '+':
                 result = num1 + num2;
@@ -20,16 +23,20 @@ public class Calculator {
             case '/':
                 if (num2 == 0) {
                     System.out.println("나눗셈 연산에서 두 번째 숫자에 0이 입력될 수 없습니다.");
+                    isChecked = true;
                     break;
                 }
                 result = num1 / num2;
                 break;
             default:
                 System.out.println("잘못된 사칙연산 기호입니다.");
+                isChecked = true;
                 break;
         }
-        // static 메서드로 써보려했으나 results가 인스턴스 필드이기때문에 static 필드로 바꿔줘야하는 문제발생
-        results.add(result);
+        // 잘못된 연산일시 연산결과에 저장x
+        if (!isChecked) {
+            results.add(result);
+        }
         return result;
     }
 
--- src/com/myapp/calculator/Main.java
+++ src/com/myapp/calculator/Main.java
@@ -10,45 +10,56 @@ public class Main {
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요 : ");
+            // 추후 예외처리?
             int num1 = sc.nextInt();
             System.out.print("두 번째 숫자를 입력하세요 : ");
             int num2 = sc.nextInt();
             System.out.print("사칙연산 기호를 입력하세요(+,-,*,/) : ");
             char oper = sc.next().charAt(0);
+            sc.nextLine();
 
             // static 메서드로 쓸때
 //            int result = Calculator.calculator(num1, num2, oper);
+            // 연산 메서드 호출
             int result = cal.calculator(num1, num2, oper);
             System.out.println("결과: " + result);
+            // 게터 메서드 사용
             ArrayList<Integer> results = cal.getResults();
             System.out.println("현재까지의 연산 결과 : " + results);
-            System.out.print("1. 첫번째 연산 결과 삭제 2. 연산 결과 직접 수정 3. 다음 계산 (exit 입력 시엔 종료) : ");
-            sc.nextLine();
-            String select = sc.nextLine();
-            System.out.println(select);
-            switch (select) {
-                case "1":
-                    cal.removeResult();
-                    break;
-                case "2":
-                    System.out.print("숫자들을 띄워쓰기해서 입력해주세요");
-                    String[] s = sc.nextLine().split(" ");
-                    results.clear();
-                    for (String str : s) {
-                        int a = Integer.parseInt(str);
-                        results.add(a);
-                    }
-                    // set은 수정하는데 쓰이지만 i번째 인덱스가 없을시 예외발생
+
+            boolean isTrue = true;
+            while (isTrue) {
+                System.out.print("1. 첫번째 연산 결과 삭제 2. 연산 결과 직접 수정 3. 다음 계산 (exit 입력 시엔 종료) : ");
+                String select = sc.nextLine();
+                switch (select) {
+                    case "1":
+                        // 첫 번째 연산결과 삭제
+                        cal.removeResult();
+                        System.out.println("삭제한 결과 : " + results);
+                        break;
+                    case "2":
+                        System.out.print("숫자들을 띄워쓰기해서 입력해주세요 : ");
+                        // 세터 메서드 사용
+                        String[] s = sc.nextLine().split(" ");
+                        results.clear();
+                        for (String str : s) {
+                            int a = Integer.parseInt(str);
+                            results.add(a);
+                        }
+                        System.out.println("수정한 결과 : " + results);
+                        // set은 수정하는데 쓰이지만 i번째 인덱스가 없을시 예외발생
 //                    for (int i=0; i<s.length; i++) {
 //                        results.set(i, Integer.parseInt(s[i]));
 //                    }
-                    cal.setResults(results);
-                    break;
-                case "exit":
-                    return;
-                default:
-                    System.out.println("default");
-                    break;
+                        cal.setResults(results);
+                        break;
+                    case "exit":
+                        System.out.println("프로그램을 종료합니다.");
+                        return;
+                    case "3":
+                        isTrue = false;
+                        break;
+                }
             }
         }
 //            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
